ef3dbb81604f30e3044ad5e9901fad454f369969
Thiago Duarte <118749389+Duartelitz@users.noreply.github.com>
2023-07-21 11:16:34 -0300

    att

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
-    $('.menu__button').click(function () {
-        $('#cabecalho__menu').slideToggle();
-    });
+  $(".menu__button").click(function () {
+    $("#cabecalho__menu").slideToggle();
+  });
+    $(".show__Text").click(function () { 
+    $('.show__Text').html("▼")
+    $(".apresentacao__colaborador").slideToggle();
+    $(this).toggleClass("show__active");
+    $(".show__active").html("▲");
+  }); 
+    
 });
